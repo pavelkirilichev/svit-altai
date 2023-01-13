de47dbb68c34443a8468d85584a1d926bda0daef
Apolinarui <veshapoli@gamil.com>
2023-01-13 10:29:40 +0300

    fix

---
+++ src/components/PersonalData.jsx
@@ -0,0 +1,16 @@
+function PersonalData() {
+    return (
+        <div className="cart__main__inner">
+            <div className="personalData__head">
+                <button className="personalData__btn-back">Назад</button>
+                <button className="personalData__btn-edit">
+                    <span>Редакт.</span>
+                    <img src="./images/personal_data/edit.svg"/>
+                </button>
+            </div>
+        </div>
+    )
+    
+}
+
+export default PersonalData
